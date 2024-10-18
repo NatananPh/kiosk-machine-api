ef6d90392124af8b8beadb4f0c62526594f7abd2
NatananPh <nut14586@gmail.com>
2024-10-18 16:19:28 +0700

    feat(server): initialize echo server

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/NatananPh/kiosk-machine-api/config"
+	"github.com/NatananPh/kiosk-machine-api/database"
+	"github.com/NatananPh/kiosk-machine-api/server"
+)
+
+func main() {
+	cfg := config.GetConfig()
+	db := database.NewPostgresDatabase(cfg.Database)
+	echoServer := server.NewEchoServer(db.Connect(), cfg)
+	echoServer.Start()
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,50 @@
 package server
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/NatananPh/kiosk-machine-api/config"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type echoServer struct {
+	app *echo.Echo
+	db *gorm.DB
+	cfg *config.Config
+}
+
+var (
+	once sync.Once
+	server *echoServer
+)
+
+func NewEchoServer(db *gorm.DB, cfg *config.Config) *echoServer {
+	echoApp := echo.New()
+	echoApp.Logger.SetLevel(log.DEBUG)
+
+	once.Do(func() {
+		server = &echoServer{
+			app: echoApp,
+			db: db,
+			cfg: cfg,
+		}
+	})
+	return server
+}
+
+func (s *echoServer) Start() {
+	s.app.GET("/health" , func(c echo.Context) error {
+		return c.String(200, "OK")
+	})
+	s.httpListening()
+}
+
+func (s *echoServer) httpListening() {
+	url := fmt.Sprintf(":%d",  s.cfg.Server.Port)
+	if err := s.app.Start(url); err != nil {
+		s.app.Logger.Fatalf("Failed to start server: %v", err)
+	}
+}
